134514407948d1805abd5db934747e8f9e64e897
Luiz Claudio <74454646+lcsm212@users.noreply.github.com>
2021-11-05 08:52:34 -0300

    inicio

---
+++ src/main/java/com/tubocerto/tubocerto/TubocertoApplication.java
@@ -0,0 +1,33 @@
+package com.tubocerto.tubocerto;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.tubocerto.tubocerto.domain.Categoria;
+import com.tubocerto.tubocerto.repositories.CategoriaRepository;
+
+@SpringBootApplication
+public class TubocertoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TubocertoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
+}
---
+++ src/test/java/com/tubocerto/tubocerto/TubocertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubocerto.tubocerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubocertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
